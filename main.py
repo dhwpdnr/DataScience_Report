87bd229b98eaa83d68e1bd2390abe40f8aa8093f
dhwpdnr <ink1237@gmail.com>
2023-12-26 01:18:11 +0900

    [feat] ime series analysis

--- main.py
+++ main.py
@@ -4,109 +4,72 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
+from statsmodels.tsa.seasonal import seasonal_decompose
 
-# 파일 경로
-file_paths = {
-    "brand_keyword_cnt": "data/brand_keyword_cnt.csv",
-    "product_info": "data/product_info.csv",
-    "sales": "data/sales.csv",
-    "sample_submission": "data/sample_submission.csv",
-    "train": "data/train.csv"
-}
+# 데이터 불러고이
+sales_data_path = 'data/sales.csv'
+sales_data = pd.read_csv(sales_data_path)
 
-# 데이터프레임으로 불러오기
-dataframes = {name: pd.read_csv(path) for name, path in file_paths.items()}
+# 데이터 구조
+print(sales_data.head())
 
-# 각 데이터프레임의 첫 5행 확인
-for name, df in dataframes.items():
-    print(f"---{name}---")
-    print(df.head())
-    print("\n")
+# 데이터를 시계열 형식으로 변환, 매일의 상품의 판매 데이터를 집계
+# 시계열의 날짜 열 추출
+date_columns = sales_data.columns[6:]  # 날짜열은 7번째 부터 시작
 
-# 결측치 확인
-for name, df in dataframes.items():
-    print(f"---{name} 결측치---")
-    print(df.isnull().sum())
-    print("\n")
+# 모든 제품의 일별 판매량을 더함
+daily_sales = sales_data[date_columns].sum()
 
-# 기술 통계 분석
-for name, df in dataframes.items():
-    # 수치형 데이터에 대한 기술 통계만 계산
-    if df.select_dtypes(include=[np.number]).shape[1] > 0:
-        print(f"---{name} 기술 통계---")
-        print(df.describe())
-        print("\n")
+# 시계열 데이터프레임 만들기
+time_series_data = pd.DataFrame({'Date': pd.to_datetime(date_columns), 'Total Sales': daily_sales.values})
 
-# 결측치 처리
-for name, df in dataframes.items():
-    df.fillna(0, inplace=True)
-import pandas as pd
+# 시계열 데이터의 행 확인
+print(time_series_data.head())
 
-# 'sales.csv' 파일 불러오기
-sales_df = pd.read_csv("data/sales.csv")
-
-# 데이터 확인
-# print(sales_df.head())
-
-# 6번째 열부터 날짜 데이터로 가정하고, melt 함수를 사용
-sales_melted = sales_df.melt(id_vars=sales_df.columns[:6],  # 첫 5개 열을 id_vars로 유지
-                             var_name='date',
-                             value_name='sales')
-
-# 'date' 열을 datetime 객체로 변환
-sales_melted['date'] = pd.to_datetime(sales_melted['date'])
-
-# 데이터 확인
-print(sales_melted.head())
-
-# 결측치를 0 또는 평균값 등으로 대체 (여기서는 0으로 대체)
-sales_melted.fillna(0, inplace=True)
+sns.set(style="whitegrid")
 
 # 시계열 데이터 시각화
-sales_melted['sales'].plot(figsize=(15, 6))
-plt.title('Sales Over Time')
-plt.xlabel('Date')
-plt.ylabel('Sales')
-# plt.show()
-
-# 예시: 'sales' 데이터의 일부 시계열 데이터 시각화
 plt.figure(figsize=(15, 6))
-sns.lineplot(data=dataframes['sales'].iloc[:, 6:16])
-plt.title('Sales Time Series for First Few Dates')
+plt.plot(time_series_data['Date'], time_series_data['Total Sales'], label='Total Sales')
+plt.title('Daily Total Sales Over Time')
 plt.xlabel('Date')
-plt.ylabel('Sales')
-plt.xticks(rotation=45)
+plt.ylabel('Total Sales')
+plt.legend()
 # plt.show()
 
+# Date 열을 인덱스로 설정
+time_series_data.set_index('Date', inplace=True)
 
-keyword_df = pd.read_csv("data/brand_keyword_cnt.csv")
-sales_df = pd.read_csv("data/sales.csv")
+# Total Sales 열을 선택해서 계절성 분해 수행
+# additive 모델을 사용, 주기는 30일
+decomposition = seasonal_decompose(time_series_data['Total Sales'], model='additive', period=30)
 
-# 키워드 데이터의 날짜 열을 행으로 변환
-# 예시 코드에서 id_vars는 첫 5개 열을 가정하고 있습니다.
+# 시각화를 위한 플롯 사이즈 설정
+plt.figure(figsize=(14, 8))
 
-keyword_melted = keyword_df.melt(id_vars=['브랜드'], var_name='date', value_name='keyword_count')
-keyword_melted['date'] = pd.to_datetime(keyword_melted['date'])
+# 원본 시계열 데이터
+plt.subplot(411)
+plt.plot(decomposition.observed, label='Original')
+plt.legend(loc='upper left')
+plt.title('Original Time Series')
 
-# 판매 데이터의 날짜 열을 행으로 변환
-sales_melted = sales_df.melt(id_vars=sales_df.columns[:6],  # 첫 5개 열을 id_vars로 유지
-                             var_name='date',
-                             value_name='sales')
+# 추세 성분
+plt.subplot(412)
+plt.plot(decomposition.trend, label='Trend')
+plt.legend(loc='upper left')
+plt.title('Trend Component')
 
-# 'date' 열을 datetime 객체로 변환
-sales_melted['date'] = pd.to_datetime(sales_melted['date'])
+# 계절성 성분
+plt.subplot(413)
+plt.plot(decomposition.seasonal, label='Seasonal')
+plt.legend(loc='upper left')
+plt.title('Seasonal Component')
 
-# 데이터 결합
-combined_df = pd.merge(keyword_melted, sales_melted, on=['브랜드', 'date'])
+# 잔차 성분
+plt.subplot(414)
+plt.plot(decomposition.resid, label='Residual')
+plt.legend(loc='upper left')
+plt.title('Residual Component')
 
-# 피어슨 상관 계수 계산
-correlation = combined_df[['keyword_count', 'sales']].corr()
-print(correlation)
-
-
-# 상관 관계 히트맵 시각화
-plt.figure(figsize=(8, 6))
-sns.heatmap(correlation, annot=True, cmap='coolwarm')
-plt.title('Correlation Between Keyword Count and Sales')
+plt.tight_layout()
 plt.show()
